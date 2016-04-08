b1b2b48e6554533f06c32948b5084bb65c79c03c
brngdsn <brn.gdsn@gmail.com>
2016-04-08 19:38:06 -0400

    Update main.ts

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,5 @@
 import {bootstrap} from 'angular2/platform/browser';
-import {provide} from 'angular2/angular2';
+import {provide} from 'angular2/core';
 import {APP_BASE_HREF} from 'angular2/router';
 import {AppComponent} from './app.component';
 
