bc8c3c7a499f156bd92daef4073be1436262b4ec
brngdsn <brn.gdsn@gmail.com>
2016-04-08 19:25:08 -0400

    Update main.ts

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,6 @@
 import {bootstrap}    from 'angular2/platform/browser';
 import {AppComponent} from './app.component';
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+  provide(APP_BASE_HREF, {useValue: '/'})
+]);
