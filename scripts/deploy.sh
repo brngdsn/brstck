05ad0e8884ad46d6e0c2c538b357aa062c68b2ae
Braun Goodson <braun.goodson@digitaslbi.com>
2016-04-08 14:32:36 -0400

    added automated deploy script, and encrypted environment variables

---
+++ scripts/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# this is after the build succeeds
+rm -rf .git/
+git init
+git config --global user.email "$USER@$HOST"
+git config --global user.name $USER
+git checkout --orphan gh-pages
+git remote add upstream "https://$GH_TOKEN@github.com/brngdsn/brstack.git"
+git add .
+git commit -m "deploy"
+git push upstream gh-pages --force --quiet > /dev/null 2>&1
